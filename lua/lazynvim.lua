9f7e912e75adfc9d9f12686bb55550188c59382b
tony-cannibal <castrovital.luis@gmail.com>
2023-04-17 15:04:57 -0600

    Small Update

--- lua/lazynvim.lua
+++ lua/lazynvim.lua
@@ -15,7 +15,17 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         "ellisonleao/gruvbox.nvim",
-        priority = 1000,
+        -- priority = 1000,
+        opts = {
+            italic = {
+                strings = false,
+                comments = false,
+                operators = false,
+                folds = false,
+            },
+            contrast = "hard", -- can be "hard", "soft" or empty string
+            transparent_mode = false,
+        }
     },
     {
         'lewis6991/gitsigns.nvim',
@@ -32,6 +42,7 @@ require("lazy").setup({
     },
     {
         'nvim-lualine/lualine.nvim',
+        event = "BufEnter",
         dependecies = {
             'nvim-tree/nvim-web-devicons'
         },
@@ -41,6 +52,7 @@ require("lazy").setup({
     },
     {
         'akinsho/bufferline.nvim',
+        event = "BufEnter",
         config = function()
             require("configs.bufferline-nvim")
         end
@@ -88,5 +100,54 @@ require("lazy").setup({
             require("configs.nvim-lsp")
         end
     },
+    {
+        "akinsho/toggleterm.nvim",
+        cmd = "ToggleTerm",
+        config = function()
+            require("configs.toggleterm-nvim")
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = { "lua", "python" },
+            sync_install = false,
+            auto_install = true,
+        },
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end,
+        config = function()
+            local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
+            if not status_ok then
+                return
+            end
 
+            treesitter.setup {
+                -- A list of parser names, or "all"
+                ensure_installed = { "lua", "python", "rust" },
+                context_commentstring = {
+                    enable = true
+                },
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = false,
+                -- List of parsers to ignore installing (for "all")
+                ignore_install = {},
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                highlight = {
+                    -- `false` will disable the whole extension
+                    enable = true,
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end
+    },
 })
