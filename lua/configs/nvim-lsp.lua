be9806b00043e18a058fe23a87bee007031854ce
tony-cannibal <castrovital.luis@gmail.com>
2023-04-17 09:56:20 -0600

    Added LSP

---
+++ lua/configs/nvim-lsp.lua
@@ -0,0 +1,92 @@
+require("mason").setup({
+    ui = {
+        border = "single",
+    }
+})
+
+
+
+local lspconfig = require("lspconfig")
+
+require('lspconfig.ui.windows').default_options = {
+    border = "single",
+}
+
+require('mason-lspconfig').setup({
+    ensure_installed = { "lua_ls" },
+})
+
+require("lsp-format").setup {}
+
+local signs = {
+    Error = "",
+    Warn = "",
+    Hint = "",
+    Info = ""
+}
+
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+local on_attach = function(client, bufnr)
+    local opts = { noremap = true, silent = true }
+    local keymap = vim.api.nvim_buf_set_keymap
+
+    require("lsp-format").on_attach(client)
+
+    keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+    keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+    keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{ async = true }<cr>", opts)
+    keymap(bufnr, "n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+    keymap(bufnr, "n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
+    keymap(bufnr, "n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    keymap(bufnr, "n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+    keymap(bufnr, "n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+    keymap(bufnr, "n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    keymap(bufnr, "n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+    keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+end
+
+
+local handlers = {
+    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'single' }),
+    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'single' }),
+}
+
+vim.diagnostic.config {
+    float = {
+        border = 'single',
+    }
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('mason-lspconfig').setup_handlers({
+    function(server_name) -- default handler (optional)
+        lspconfig[server_name].setup {
+            on_attach = on_attach,
+            handlers = handlers,
+            capabilities = capabilities,
+        }
+    end,
+    ["lua_ls"] = function()
+        lspconfig.lua_ls.setup {
+            on_attach = on_attach,
+            handlers = handlers,
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        -- Get the language server to recognize the `vim` global
+                        globals = { 'vim' },
+                    },
+                },
+            },
+        }
+    end
+})
--- lua/lazynvim.lua
+++ lua/lazynvim.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     {
         "ellisonleao/gruvbox.nvim",
-        priority=1000,
+        priority = 1000,
     },
     {
         'lewis6991/gitsigns.nvim',
@@ -25,15 +25,68 @@ require("lazy").setup({
         end
     },
     {
-        "rebelot/heirline.nvim",
-        event = "BufEnter",
-        dependencies = {
-            { "kyazdani42/nvim-web-devicons" },
+        "numToStr/Comment.nvim",
+        config = function()
+            require("configs.comment-nvim")
+        end
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependecies = {
+            'nvim-tree/nvim-web-devicons'
         },
         config = function()
-            require("configs.userline")
+            require("configs.lualine-nvim")
         end
-    }
+    },
+    {
+        'akinsho/bufferline.nvim',
+        config = function()
+            require("configs.bufferline-nvim")
+        end
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        cmd = "NvimTreeToggle",
+        config = function()
+            require("configs.nvim-tree")
+        end
+    },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("configs.autopairs")
+        end
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'hrsh7th/nvim-cmp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            "rafamadriz/friendly-snippets",
+            "onsails/lspkind.nvim"
+        },
+        config = function()
+            require("configs.nvim-cmp")
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+            "lukas-reineke/lsp-format.nvim",
+        },
+        -- event = "BufEnter",
+        config = function()
+            require("configs.nvim-lsp")
+        end
+    },
+
 })
-
-
