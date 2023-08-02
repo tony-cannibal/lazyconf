e4b70f927695ef2912862147e45022ebb60c5629
tony-cannibal <castrovital.luis@gmail.com>
2023-08-01 19:59:37 -0600

    Updated nvimTree config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -6,7 +6,17 @@ return {
         local nvim_tree_config = require("nvim-tree.config")
         local tree_cb = nvim_tree_config.nvim_tree_callback
 
+        local function on_attach(bufnr)
+            local api = require('nvim-tree.api')
+
+            local function opts(desc)
+                return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+            end
+            vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+        end
+
         nvim_tree.setup {
+            on_attach = on_attach,
             update_focused_file = {
                 enable = true,
                 update_cwd = true,
@@ -55,13 +65,13 @@ return {
             view = {
                 width = 30,
                 side = "left",
-                mappings = {
-                    list = {
-                        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-                        { key = "h",                  cb = tree_cb "close_node" },
-                        { key = "v",                  cb = tree_cb "vsplit" },
-                    },
-                },
+                -- mappings = {
+                --     list = {
+                --         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+                --         { key = "h",                  cb = tree_cb "close_node" },
+                --         { key = "v",                  cb = tree_cb "vsplit" },
+                --     },
+                -- },
             },
             actions = {
                 open_file = {
